21beffc2a2c8dfff715844233cac6bc1d6e31150
= <ShintaoMonk@gmail.com>
2018-05-14 11:46:53 -0400

    started base code

--- updater.sh
+++ updater.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 file=$((pwd))'/repositories'
+
+if [ -e $file ]
+then
+  cat $file | while read line
+  do
+    if [[ $line != "#"* ]]
+    then
+      directory
+      link
+      action
+      for word in $line
+      do
+        echo $line
+      done
+    fi
+  done
+else
+  echo "#Use as following:" > repositories
+  echo "#{directory} {github link} {action = pull or push}" >> repositories
+fi
